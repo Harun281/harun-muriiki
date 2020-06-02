448e9ded7fc2158aee5f4a55b08c1b1a613779c5
Harun <61190057+Harun281@users.noreply.github.com>
2020-06-03 00:53:00 +0300

    update v2.1
    Rectified display of desktop screen

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-    $('').hide();
-});
+//$(document).ready(function(){
+  //  $('').hide();
+//});
+
+
+
+//hide navbar on scroll
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function(){
+    var currentScrollPos = window.pageYOffset;
+    if(this.prevScrollPos > currentScrollPos){
+        document.getElementsByClassName('mynav').style.top = '0';
+
+    }else{
+        document.getElementsByClassName('mynav').style.top = '-50px';
+    }
+    prevScrollPos = currentScrollPos;
+};
