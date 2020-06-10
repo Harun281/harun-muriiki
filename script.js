bb61e4b0f11f7726e11d81fe2bfff919c8e391ad
Harun <61190057+Harun281@users.noreply.github.com>
2020-06-10 23:53:18 +0300

    update v2.2
    Added responsiveness

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
   //  $('').hide();
 //});
 
-
+document.getElementById('projects').innerHTML = '';
 
 //hide navbar on scroll
 var prevScrollPos = window.pageYOffset;
-window.onscroll = function(){
+
+
+window.addEventListener('scroll', () =>{
     var currentScrollPos = window.pageYOffset;
     if(this.prevScrollPos > currentScrollPos){
         document.getElementsByClassName('mynav').style.top = '0';
 
     }else{
-        document.getElementsByClassName('mynav').style.top = '-50px';
+        document.getElementsByClassName('.mynav').style.top = '-50px';
     }
     prevScrollPos = currentScrollPos;
-};
+});
